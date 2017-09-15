d7bda18ba99a13276aed60a4cc5d3bd9b9a69501
Philip Whitehouse <philip@whiuk.com>
2017-09-15 02:38:44 +0100

    Fix plural strings for ICalendarUtils

--- qmail/src/main/java/com/fsck/k9/ui/messageview/ical/ICalendarUtils.java
+++ qmail/src/main/java/com/fsck/k9/ui/messageview/ical/ICalendarUtils.java
@@ -12,21 +12,22 @@ public class ICalendarUtils {
 
     static String buildRule(Recurrence recurrence, Resources resources) {
         Frequency frequency = recurrence.getFrequency();
+        Integer interval = recurrence.getInterval();
         switch (frequency) {
             case SECONDLY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_second, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_second, interval, interval);
             case MINUTELY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_minute, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_minute, interval, interval);
             case HOURLY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_hourly, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_hourly, interval, interval);
             case DAILY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_daily, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_daily, interval, interval);
             case WEEKLY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_weekly, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_weekly, interval, interval);
             case MONTHLY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_monthly, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_monthly, interval, interval);
             case YEARLY:
-                return resources.getQuantityString(R.plurals.ical_recurrence_yearly, recurrence.getInterval());
+                return resources.getQuantityString(R.plurals.ical_recurrence_yearly, interval, interval);
         }
         return "";
     }
